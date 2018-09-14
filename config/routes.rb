b1f6558f45ebcc969400ec818b12a9d59246c7a8
Rossukhon Leagmongkol <rose.roko@gmail.com>
2018-09-14 10:46:06 +0700

    Add register feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'static#index'
+  resources :users, only: [:new, :create]
 
   resources :sessions, only: [:new, :create, :destroy]
+
+  root to: 'static#index'
 end
